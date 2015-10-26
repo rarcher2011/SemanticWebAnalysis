e546ddeb37a43b79fcfa3856a84e284cd871bab7
rarcher2011 <richardjamesarcher@gmail.com>
2015-10-26 01:18:26 +0000

    Version 0.1 of this python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "SemWeb",
+    version = "0.1",
+    author = "Rich Archer",
+    author_email = "richardjamesarcher@gmail.com",
+    description = ("Package for the storage of semantic web data"),
+    license = "BSD",
+    keywords = "semantic web",
+    url = "",
+    packages=['SemWeb'],
+    
+)
